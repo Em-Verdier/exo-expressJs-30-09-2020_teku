e8a4903154392d3779507b2dedc9c0e842d048d0
Em-Verdier <verdierem@gmail.com>
2020-09-30 23:34:34 +0200

    Final

--- src/testServer.js
+++ src/testServer.js
@@ -1,20 +1,27 @@
 import fs from 'fs/promises'
 import axios from 'axios'
 
-try {
-    let response = await axios.get(
-        'http://localhost:7777/',
-        'http://localhost:7777/aboutyou',
-        'http://localhost:7777/aboutme',
-        'http://localhost:7777/vote',
-        'http://localhost:7777/vote/:age',
-        'http://localhost:7777/vote',
-        'http://localhost:7777/palindrome/:word',
-        'http://localhost:7777/oddstest/:num'
-    )
+/*try {
+    let response = await axios.get('http://localhost:7777/')
+
     let content = response.data
-    await fs.writeFile(file, content)
+    await fs.writeFile('app.js', content)
     console.log(response.data)
 } catch (e) {
     console.error(e)
 }
+*/
+let data = await Promise.all([
+    axios.get('http://localhost:7777/').then(() => 'Home'),
+    axios.get('http://localhost:7777/aboutme').then(() => 'Aboutme'),
+    axios.get('http://localhost:7777/aboutyou').then(() => 'Aboutyou'),
+    axios.get('http://localhost:7777/vote').then(() => 'VoteCheckRules'),
+    axios.get('http://localhost:7777/vote/:age').then(() => 'VoteEnterAge'),
+    axios
+        .get('http://localhost:7777/palindrome/:word')
+        .then(() => 'IsPalindrome'),
+    axios.get('http://localhost:7777/oddtest/:num').then(() => 'oddTest'),
+]).catch((err) => {
+    console.error(err)
+})
+console.log(data)
